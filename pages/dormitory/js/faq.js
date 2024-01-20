f0ba03451ee338f227084bede563febb47278909
AreaEffectCloud <84448503+AreaEffectCloud@users.noreply.github.com>
2024-01-20 13:06:11 +0900

    all done FAQ card in the dormitory page

--- pages/dormitory/js/faq.js
+++ pages/dormitory/js/faq.js
@@ -1,13 +1,21 @@
-
 var faqQuestions = document.getElementsByClassName("faq-item-contents");
 
 for (var i = 0; i < faqQuestions.length; i++) {
     faqQuestions[i].addEventListener("click", function() {
-
+        // faq-item-text
+        var faqImage = this.children[0];
+        if (faqImage.style.color === 'rgb(0, 44, 244)') {
+            faqImage.style.color = 'rgb(0, 0, 0)';
+        } else {
+            faqImage.style.color = 'rgb(0, 44, 244)';
+        }
         // faq-item-img
         var faqImage = this.children[1];
-        faqImage.classList.toggle('active');
-        
+        if (faqImage.style.transform) {
+            faqImage.style.transform = null;
+        } else {
+            faqImage.style.transform = 'rotate(180deg)';
+        }
         // faq-item-answer
         var faqAnswer = this.nextElementSibling;
         if (faqAnswer.style.maxHeight) {
